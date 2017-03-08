d90b6003caf6f73031076222585aeba37926fa58
Tyler Black Hernandez <tylerblackhernandez@gmail.com>
2017-03-08 11:07:18 -0600

    Setup creating tweets. Add crud routes for tweets. Add new and create methods to tweets controller. Create form in new tweets view. setup strong params for tweets

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,33 @@
+class TweetsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    respond_to do |format|
+      if @tweet.save
+        format.html {redirect_to @tweet, notice: 'Tweet was successfully created!'}
+      else
+        format.html {render :new}
+      end
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:message, :user_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  resources :tweets
+
+  get 'tweets/index'
+
+  get 'tweets/show'
+
+  get 'tweets/new'
+
+  get 'tweets/edit'
+
+  root 'tweets#index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
