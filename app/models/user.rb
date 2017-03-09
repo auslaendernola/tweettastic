b94ba9e9014faae1a6373734747260e30174583c
Tyler Black Hernandez <tylerblackhernandez@gmail.com>
2017-03-09 09:03:45 -0600

    Add validations for tweets and user accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, presence: true, uniqueness: true
 end
