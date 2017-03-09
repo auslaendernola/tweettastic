5fb27edda5a9dd1dc5d5103fc9201685c6b1a4df
Tyler Black Hernandez <tylerblackhernandez@gmail.com>
2017-03-09 10:03:55 -0600

    Add relationships table and setup models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :tweets
+  has_many :relationships
+  has_many :friends, through: :relationships
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
